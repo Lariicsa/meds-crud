f777d90b0431ecd018a8878eaff30f1ad601267d
Larissa <dejamepienso@gmail.com>
2019-07-08 13:54:21 -0500

    Fix layout on Ingreso View

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,21 @@
 <template>
   <div id="app" class="container">
-    <nav class="navbar" role="navigation" aria-label="main navigation">
-      <div class="navbar-brand">
-        <router-link :to="{name: 'inicio'}" class="navbar-item">
-          <h2 class="title is-3">Homeds</h2>
-        </router-link>
-        <a
-          role="button"
-          class="navbar-burger burger"
-          aria-label="menu"
-          aria-expanded="false"
-          data-target="navbarBasicExample"
-        >
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </a>
-      </div>
-      <div class="navbar-menu">
-        <div class="navbar-end">
-          <div class="navbar-item">
-            <div class="buttons">
-              <strong class="navbar-item">{{usuario.email}}</strong>
-              <a class="button is-light" v-if="existeUsuario" @click="cerrarSesion">Cerrar sesión</a>
-              <router-link v-if="!existeUsuario" :to="{name: 'ingreso'}" class="button is-light">
-                <a>Login</a>
-              </router-link>
-              <router-link v-if="!existeUsuario" :to="{name: 'registro'}" class="button is-primary">
-                <strong>Register</strong>
-              </router-link>
-            </div>
-          </div>
-        </div>
-      </div>
-    </nav>
+    <Header/>
     <hr>
-    <router-view/>
+    <section>
+      <router-view/>
+    </section>
   </div>
 </template>
 
 <script>
 import { mapActions, mapGetters, mapState } from "vuex";
+import Header from './components/Header';
 export default {
   name: "App",
+  components:{
+    Header
+  },
   methods: {
     ...mapActions(["cerrarSesion"])
   },
@@ -52,3 +25,7 @@ export default {
   }
 };
 </script>
+
+<style>
+@import "./scss/shared.scss";
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,55 @@
+<template>
+<nav class="navbar" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <router-link :to="{name: 'inicio'}" class="navbar-item">
+          <h2 class="title is-3">Homeds</h2>
+        </router-link>
+        <a
+          role="button"
+          class="navbar-burger burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+      <div class="navbar-menu">
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <div class="buttons">
+              <strong v-if="existeUsuario" class="navbar-item">{{usuario.email}}</strong>
+              <a class="button is-light" v-if="existeUsuario" @click="cerrarSesion">Cerrar sesión</a>
+              <router-link v-if="!existeUsuario" :to="{name: 'ingreso'}" class="button is-light">
+                <a>Login</a>
+              </router-link>
+              <router-link v-if="!existeUsuario" :to="{name: 'registro'}" class="button is-primary">
+                <strong>Register</strong>
+              </router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+</template>
+<script>
+import { mapActions, mapState, mapGetters } from "vuex";
+export default {
+	name: 'Header',
+	data(){
+		return{
+			email:'',
+			pass: ''
+		}
+	},
+	methods: {
+    ...mapActions(["cerrarSesion"])
+  },
+  computed: {
+    ...mapGetters(["existeUsuario"]),
+    ...mapState(["usuario", "error"])
+  }
+}
+</script>
